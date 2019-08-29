22b755261b954cc808a273410dbdd434057fc2dc
ozhangulal <ozhangulal@outlook.com>
2019-08-29 14:39:42 +0300

    DotNetCoreFundamentalsProject
    Drilling into Data,Working with Models and Model Binding

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,73 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        //IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        int Commit();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{ Id=1,Name="Scott's Pizza",Location="MaryLand",Cuisine=CuisineType.Italian},
+                new Restaurant{ Id=2,Name="Cinnamon Club",Location="New York",Cuisine=CuisineType.None},
+                new Restaurant{ Id=3,Name="Calypso",Location="Virginia Beach",Cuisine=CuisineType.Mexican},
+                new Restaurant{ Id=4,Name="Buenos Aires",Location="Norfolk",Cuisine=CuisineType.Indian},
+                new Restaurant{ Id=5,Name="Keen's SteakHouse",Location="Washington D.C",Cuisine=CuisineType.Mexican}
+            };
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name=null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name)||r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+    }
+}
---
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        public string Message { get; set; }
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        [BindProperty(SupportsGet =true)] /*[BindProperty] attribute’unu kullanarak data modellerimizi sayfaya bağlamak mümkün. Bu sayede HTML tarafında bu özelliği kullanarak data modelinize ulaşmanız ve verileri taşımanız mümkün. MVC ile çok bir farkı yok, benzer konsept.*/
+        public string SearchTerm { get; set; }
+
+        public ListModel( IConfiguration config,
+                          IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
+        public void OnGet(string SearchTerm)
+        {
+
+            //Message = "Hello, World !";
+            SearchTerm = SearchTerm;
+            Message = config["Message"];
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
+        }
+    }
+}
